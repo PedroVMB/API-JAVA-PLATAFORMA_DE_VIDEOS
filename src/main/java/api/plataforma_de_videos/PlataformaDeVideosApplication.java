c3177ba17fe0680fded399e5a6f0a94a6090ac20
PedroVMB <pedromb.dev@gmail.com>
2023-09-26 18:17:21 -0300

    feat: first commit with app.properties configurated

---
+++ src/main/java/api/plataforma_de_videos/PlataformaDeVideosApplication.java
@@ -0,0 +1,13 @@
+package api.plataforma_de_videos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeVideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeVideosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/plataforma_de_videos/PlataformaDeVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package api.plataforma_de_videos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
